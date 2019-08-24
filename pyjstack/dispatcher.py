68c13ce115313c977e4c94f7c44f155169b05600
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-08-23 20:35:36 -0700

    Email specific code changes for pyjstack

---
+++ pyjstack/dispatcher.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+def connect(smtp_server, smtp_port, email, password):
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(email, password)
+    return server
+
+def attach(path):
+    filename = os.path.basename(path)
+    attachment = open(path, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+
+def send(server, from_email, to_email, subject, content, attachment):
+    message = MIMEMultipart()
+    message['From'] = from_email
+    message['To'] = to_email
+    message['Subject'] = subject
+
+    message.attach(MIMEText(content, 'html'))
+    message.attach(attachment)
+
+    server.sendmail(from_email, to_email, message.as_string()) 
+    server.quit()

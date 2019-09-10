f66114d3fee799817185e1b13ccee8505a837809
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-09-09 22:53:41 -0700

    SFTP Configuration details for shipping the thread dump package.

--- tests/test_xml_conf.py
+++ tests/test_xml_conf.py
@@ -1,3 +1,9 @@
 import unittest
 
 from pyjstack.sysconfig import read_xml_conf
+
+
+class TestXMLConfMethods(unittest.TestCase):
+
+    def test_read_json_conf(self):
+        conf = read_xml_conf()

3e839abc112ff508e484be1a0d50d04a77117aca
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-09-09 22:55:27 -0700

    SFTP Configuration details for shipping the thread dump package.

--- tests/test_yml_conf.py
+++ tests/test_yml_conf.py
@@ -8,5 +8,5 @@ CONF_PATH = 'conf/pyjstack-conf.yml'
 class TestYMLConfMethods(unittest.TestCase):
 
     def test_read_json_conf(self):
-        conf = read_yaml_conf()
+        conf = read_yaml_conf(CONF_PATH)
 

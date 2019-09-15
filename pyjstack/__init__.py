43bafa6b8e953b4bd253468bb1a1d20331501ac3
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-09-15 11:28:03 -0700

    SFTP Configuration details for shipping the thread dump package.

--- pyjstack/__init__.py
+++ pyjstack/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 
 CONF_XML_PATH = 'conf/pyjstack-conf.xml'
+CONF_YML_PATH = 'conf/pyjstack-conf.yml'
+CONF_INI_PATH = 'conf/pyjstack-conf.ini'
+CONF_JSON_PATH = 'conf/pyjstack-conf.json'

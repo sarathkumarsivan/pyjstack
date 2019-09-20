3074287ac42ed38b8f62493cfceaf292d010100a
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-09-19 23:19:42 -0700

    SFTP Configuration details for shipping the thread dump package.

--- tests/test_yml_conf.py
+++ tests/test_yml_conf.py
@@ -10,4 +10,4 @@ class TestYMLConfMethods(unittest.TestCase):
         conf = read_yaml_conf(CONF_YML_PATH)
         self.assertEqual(conf['email']['email'], 'user@gmail.com')
         self.assertEqual(conf['email']['password'], 'secret')
-
+        self.assertEqual(conf['email']['smptp_server'], 'smtp.cloud.com')
